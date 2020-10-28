e72ef0bfd4dee26c8d2d98aba700d3283cc6ec07
wangdaofeng <2161783757@qq.com>
2020-10-28 10:11:23 +0800

    update class and onject

--- 类和对象/2020.10.27_3.kt
+++ 类和对象/2020.10.27_3.kt
@@ -1,17 +1,17 @@
 package com.example.android.roomwordssample
 
 open class Rectangle(name:String){
-    val fillColor:String
+    /*private*/ val fillColor:String
      get() = "blue"
 
-    fun doFillRectangle() {
+    /*private*/ fun doFillRectangle() {
         println("This is base fillRectangle")
     }
 }
 
 /**
  * 使用super关键字可以访问超类中的函数和属性访问器。
- * 这个super关键字可以访问的函数和属性是否可以重写不存在任何的关联
+ * 这个super关键字可以访问的函数和属性是否可以重写不存在任何的关联,只需要存在继承关系并且可见性修饰符不为private时就可以访问到。
  */
 class FilledRectangle(name:String) :Rectangle(name){
     val filledRectangleColor = super.fillColor
@@ -20,10 +20,42 @@ class FilledRectangle(name:String) :Rectangle(name){
         super.doFillRectangle()
         println("My Code!")
     }
+    inner class InnerFilledRectangle() {
+        fun innerFilledRectangle() {
+            println("inner class")
+            super@FilledRectangle.doFillRectangle()
+        }
+        val innerObject:String
+            get() = super@FilledRectangle.fillColor
+    }
+}
+
+open class Rectangle2{
+    open fun draw() {
+        println("This is Rectangle2's draw")
+    }
+}
+
+interface Polygon {
+    fun draw() {
+        println("This is Polygon's draw")
+    }
+}
+
+/**
+ * 继承、实现 中存在函数名称冲突时super关键字需要加上限定字
+ */
+class MyClass:Rectangle2(), Polygon {
+    override fun draw() {
+        super<Rectangle2>.draw()
+    }
 }
 
 fun main() {
-    println(FilledRectangle("XName").filledRectangleColor)
-    FilledRectangle("YName").doFilledRectangleColorFun()
+//    println(FilledRectangle("XName").filledRectangleColor)
+//    println(FilledRectangle("YName").InnerFilledRectangle().innerObject)
+//    FilledRectangle("YName").InnerFilledRectangle().innerFilledRectangle()
+      MyClass().draw()
 }
+
 
